1d3cd1a381bbfc32de2974240c548605e823ef29
Sayed Elhawary <selhawary2025@hotmail.com>
2025-04-19 19:27:01 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from app2')
+    res.send('Hello World from app')
   })
   
-  app.listen(port, () => {
+  app.listen(port, () => {2
     console.log(`Example app listening at http://localhost:${port}`)
   })
