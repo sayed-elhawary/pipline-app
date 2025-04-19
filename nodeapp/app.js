7529f5f340f8b0498b02352ef8b8d1fcb7f517de
Sayed Elhawary <selhawary2025@hotmail.com>
2025-04-19 19:14:41 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from ITI 5')
+    res.send('Hello World from ITI 55')
   })
   
   app.listen(port, () => {
